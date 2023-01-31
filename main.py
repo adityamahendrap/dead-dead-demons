357ee6b92fb60d0d7d4913d8896c7a81ac6bdafa
adityamahendrap <ptadityamahendrap@gmail.com>
2023-01-31 18:41:57 +0800

    add high score

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pygame
+import pygame as py
 from random import randint
 
 def displayScore():
-    currentTime = int(pygame.time.get_ticks()/100) - startTime
+    currentTime = int(py.time.get_ticks()/100) - startTime
     scoreSurf = font.render(f'{currentTime}', True, "white")
     scoreSurf.set_alpha(150)
     scoreRect = scoreSurf.get_rect(topleft = (5, 5))
@@ -47,13 +47,24 @@ def checkCol(player, obs):
             if player.colliderect(obsRect): 
                 return False
     return True
+    
+def introScreen():
+    screen.blit(introSurf, introRect)
+    screen.blit(playSurf, playRect)
+    screen.blit(highSoreSurf, highSoreRect)
 
-pygame.mixer.init(44100, -16, 2, 64)
-pygame.init()
+def gameOverScreen():
+    screen.blit(overSurf, overRect)
+    screen.blit(scoreMsgSurf, scoreMsgRect)
+    screen.blit(playSurf2, playRect2)
+    screen.blit(highSoreSurf, highSoreRect2)
+    
+py.mixer.init(44100, -16, 2, 64)
+py.init()
 WIDTH, HEIGHT = 800, 400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Dead Dead Demons")
-clock = pygame.time.Clock()
+screen = py.display.set_mode((WIDTH, HEIGHT))
+py.display.set_caption("Ghost Yeeter")
+clock = py.time.Clock()
 gameActive = False
 startTime = 0
 score = 0
@@ -63,52 +74,59 @@ walkCount = 0
 onGround = 325
 playerGravity = 0
 
+# Read High Score
+f = open("data/high_score.txt", "r")
+if f.readline() is not '':
+    highScore = int(f.readline())
+else: highScore = 0
+f.close()
+
 # Environment.
-skySurf = pygame.image.load("bg/background.png").convert()
-skySurf = pygame.transform.scale(skySurf, (928 *1.25, 431 *1.25))
-groundSurf = pygame.image.load("bg/floor1.png").convert_alpha()
-groundSurf2 = pygame.image.load("bg/floor2.png").convert_alpha()
+skySurf = py.image.load("assets/img/bg/background.png").convert()
+skySurf = py.transform.scale(skySurf, (928 *1.25, 431 *1.25))
+groundSurf = py.image.load("assets/img/bg/floor1.png").convert_alpha()
+groundSurf2 = py.image.load("assets/img/bg/floor2.png").convert_alpha()
 
 # Font.
-font = pygame.font.Font("font/SFAtarian.ttf", 35)
-fontPlay = pygame.font.Font("font/SFAtarian.ttf", 20)
-overFont = pygame.font.Font("font/SFAtarian.ttf", 125)
+font = py.font.Font("assets/font/SFAtarian.ttf", 35)
+fontPlay = py.font.Font("assets/font/SFAtarian.ttf", 20)
+overFont = py.font.Font("assets/font/SFAtarian.ttf", 125)
 
 # Player.
-playerSurf_R = pygame.image.load("sprite/playerR.png").convert_alpha()
-playerWalk_R1 = pygame.image.load("sprite/walk/R/walk1.png").convert_alpha()
-playerWalk_R2 = pygame.image.load("sprite/walk/R/walk2.png").convert_alpha()
-playerWalk_R3 = pygame.image.load("sprite/walk/R/walk3.png").convert_alpha()
-playerWalk_R4 = pygame.image.load("sprite/walk/R/walk4.png").convert_alpha()
-playerWalk_R5 = pygame.image.load("sprite/walk/R/walk5.png").convert_alpha()
-playerWalk_R6 = pygame.image.load("sprite/walk/R/walk6.png").convert_alpha()
-playerWalk_R7 = pygame.image.load("sprite/walk/R/walk7.png").convert_alpha()
-playerWalk_R8 = pygame.image.load("sprite/walk/R/walk8.png").convert_alpha()
-playerWalk_R9 = pygame.image.load("sprite/walk/R/walk9.png").convert_alpha()
-playerWalk_R10 = pygame.image.load("sprite/walk/R/walk10.png").convert_alpha()
-playerWalk_R11 = pygame.image.load("sprite/walk/R/walk11.png").convert_alpha()
-playerWalk_R12 = pygame.image.load("sprite/walk/R/walk12.png").convert_alpha()
+playerSurf_R = py.image.load("assets/img/sprite/playerR.png").convert_alpha()
+playerWalk_R1 = py.image.load("assets/img/sprite/walk/R/walk1.png").convert_alpha()
+playerWalk_R2 = py.image.load("assets/img/sprite/walk/R/walk2.png").convert_alpha()
+playerWalk_R3 = py.image.load("assets/img/sprite/walk/R/walk3.png").convert_alpha()
+playerWalk_R4 = py.image.load("assets/img/sprite/walk/R/walk4.png").convert_alpha()
+playerWalk_R5 = py.image.load("assets/img/sprite/walk/R/walk5.png").convert_alpha()
+playerWalk_R6 = py.image.load("assets/img/sprite/walk/R/walk6.png").convert_alpha()
+playerWalk_R7 = py.image.load("assets/img/sprite/walk/R/walk7.png").convert_alpha()
+playerWalk_R8 = py.image.load("assets/img/sprite/walk/R/walk8.png").convert_alpha()
+playerWalk_R9 = py.image.load("assets/img/sprite/walk/R/walk9.png").convert_alpha()
+playerWalk_R10 = py.image.load("assets/img/sprite/walk/R/walk10.png").convert_alpha()
+playerWalk_R11 = py.image.load("assets/img/sprite/walk/R/walk11.png").convert_alpha()
+playerWalk_R12 = py.image.load("assets/img/sprite/walk/R/walk12.png").convert_alpha()
 playerFrames_R = [playerWalk_R1, playerWalk_R2, playerWalk_R3, playerWalk_R4, playerWalk_R5, playerWalk_R6, playerWalk_R7, playerWalk_R8, playerWalk_R9, playerWalk_R10, playerWalk_R11, playerWalk_R12]
 playerFramesIndex_R = 0
 
-playerSurf_L = pygame.image.load("sprite/playerL.png").convert_alpha()
-playerWalk_L1 = pygame.image.load("sprite/walk/L/walk1.png").convert_alpha()
-playerWalk_L2 = pygame.image.load("sprite/walk/L/walk2.png").convert_alpha()
-playerWalk_L3 = pygame.image.load("sprite/walk/L/walk3.png").convert_alpha()
-playerWalk_L4 = pygame.image.load("sprite/walk/L/walk4.png").convert_alpha()
-playerWalk_L5 = pygame.image.load("sprite/walk/L/walk5.png").convert_alpha()
-playerWalk_L6 = pygame.image.load("sprite/walk/L/walk6.png").convert_alpha()
-playerWalk_L7 = pygame.image.load("sprite/walk/L/walk7.png").convert_alpha()
-playerWalk_L8 = pygame.image.load("sprite/walk/L/walk8.png").convert_alpha()
-playerWalk_L9 = pygame.image.load("sprite/walk/L/walk9.png").convert_alpha()
-playerWalk_L10 = pygame.image.load("sprite/walk/L/walk10.png").convert_alpha()
-playerWalk_L11 = pygame.image.load("sprite/walk/L/walk11.png").convert_alpha()
-playerWalk_L12 = pygame.image.load("sprite/walk/L/walk12.png").convert_alpha()
+playerSurf_L = py.image.load("assets/img/sprite/playerL.png").convert_alpha()
+playerWalk_L1 = py.image.load("assets/img/sprite/walk/L/walk1.png").convert_alpha()
+playerWalk_L2 = py.image.load("assets/img/sprite/walk/L/walk2.png").convert_alpha()
+playerWalk_L3 = py.image.load("assets/img/sprite/walk/L/walk3.png").convert_alpha()
+playerWalk_L4 = py.image.load("assets/img/sprite/walk/L/walk4.png").convert_alpha()
+playerWalk_L5 = py.image.load("assets/img/sprite/walk/L/walk5.png").convert_alpha()
+playerWalk_L6 = py.image.load("assets/img/sprite/walk/L/walk6.png").convert_alpha()
+playerWalk_L7 = py.image.load("assets/img/sprite/walk/L/walk7.png").convert_alpha()
+playerWalk_L8 = py.image.load("assets/img/sprite/walk/L/walk8.png").convert_alpha()
+playerWalk_L9 = py.image.load("assets/img/sprite/walk/L/walk9.png").convert_alpha()
+playerWalk_L10 = py.image.load("assets/img/sprite/walk/L/walk10.png").convert_alpha()
+playerWalk_L11 = py.image.load("assets/img/sprite/walk/L/walk11.png").convert_alpha()
+playerWalk_L12 = py.image.load("assets/img/sprite/walk/L/walk12.png").convert_alpha()
 playerFrames_L = [playerWalk_L1, playerWalk_L2, playerWalk_L3, playerWalk_L4, playerWalk_L5, playerWalk_L6, playerWalk_L7, playerWalk_L8, playerWalk_L9, playerWalk_L10, playerWalk_L11, playerWalk_L12]
 playerFramesIndex_L = 0
 
-playerJump_R = pygame.image.load("sprite/jump/jumpR.png").convert_alpha()
-playerJump_L = pygame.image.load("sprite/jump/jumpL.png").convert_alpha()
+playerJump_R = py.image.load("assets/img/sprite/jump/jumpR.png").convert_alpha()
+playerJump_L = py.image.load("assets/img/sprite/jump/jumpL.png").convert_alpha()
 
 playerRect = playerSurf_R.get_rect(midbottom= (WIDTH/2, onGround))
 playerImage = playerSurf_R
@@ -117,33 +135,33 @@ idleSurf = playerSurf_R
 idleRect = idleSurf.get_rect(midbottom= (WIDTH/2 - 3, onGround))
 
 # Obstacles.
-mobFrame_R1 = pygame.image.load("sprite/mobR.png").convert_alpha()
+mobFrame_R1 = py.image.load("assets/img/sprite/mobR.png").convert_alpha()
 mobFrame_R1.set_alpha(150)
-mobFrame_R2 = pygame.image.load("sprite/mobR2.png").convert_alpha()
+mobFrame_R2 = py.image.load("assets/img/sprite/mobR2.png").convert_alpha()
 mobFrame_R2.set_alpha(150)
 mobFrames_R = [mobFrame_R1, mobFrame_R2]
 mobFramesIndex_R = 0
 mobSurf_R = mobFrames_R[mobFramesIndex_R]
 
-mobFrame_L1 = pygame.image.load("sprite/mobL.png").convert_alpha()
+mobFrame_L1 = py.image.load("assets/img/sprite/mobL.png").convert_alpha()
 mobFrame_L1.set_alpha(150)
-mobFrame_L2 = pygame.image.load("sprite/mobL2.png").convert_alpha()
+mobFrame_L2 = py.image.load("assets/img/sprite/mobL2.png").convert_alpha()
 mobFrame_L2.set_alpha(150)
 mobFrames_L = [mobFrame_L1, mobFrame_L2]
 mobFramesIndex_L = 0
 mobSurf_L = mobFrames_L[mobFramesIndex_L]
 
-flyFrame_R1 = pygame.image.load("sprite/flyR.png").convert_alpha()
+flyFrame_R1 = py.image.load("assets/img/sprite/flyR.png").convert_alpha()
 flyFrame_R1.set_alpha(150)
-flyFrame_R2 = pygame.image.load("sprite/flyR2.png").convert_alpha()
+flyFrame_R2 = py.image.load("assets/img/sprite/flyR2.png").convert_alpha()
 flyFrame_R2.set_alpha(150)
 flyFrames_R = [flyFrame_R1, flyFrame_R2]
 flyFramesIndex_R = 0
 flySurf_R = flyFrames_R[flyFramesIndex_R]
 
-flyFrame_L1 = pygame.image.load("sprite/flyL.png").convert_alpha()
+flyFrame_L1 = py.image.load("assets/img/sprite/flyL.png").convert_alpha()
 flyFrame_L1.set_alpha(150)
-flyFrame_L2 = pygame.image.load("sprite/flyL2.png").convert_alpha()
+flyFrame_L2 = py.image.load("assets/img/sprite/flyL2.png").convert_alpha()
 flyFrame_L2.set_alpha(150)
 flyFrames_L = [flyFrame_L1, flyFrame_L2]
 flyFramesIndex_L = 0
@@ -153,53 +171,53 @@ obsRectList = []
 obsRectListL = []
 
 # Sound.
-jumpSound = pygame.mixer.Sound("sound/jump.mp3")
-music = pygame.mixer.music.load("sound/forest.mp3")
-pygame.mixer.music.play(-1)
+jumpSound = py.mixer.Sound("assets/sound/jump.mp3")
+music = py.mixer.music.load("assets/sound/forest.mp3")
+py.mixer.music.play(-1)
 # Timer.
-obsTimer = pygame.USEREVENT + 1
-pygame.time.set_timer(obsTimer, randint(1400, 2500))
+obsTimer = py.USEREVENT + 1
+py.time.set_timer(obsTimer, randint(1400, 2500))
 
-flyAnimationTimer = pygame.USEREVENT + 2
-pygame.time.set_timer(flyAnimationTimer, randint(300, 500))
+flyAnimationTimer = py.USEREVENT + 2
+py.time.set_timer(flyAnimationTimer, randint(300, 500))
 
-flyAnimationTimer2 = pygame.USEREVENT + 3
-pygame.time.set_timer(flyAnimationTimer2, randint(300, 500))
+flyAnimationTimer2 = py.USEREVENT + 3
+py.time.set_timer(flyAnimationTimer2, randint(300, 500))
 
-mobAnimationTimer = pygame.USEREVENT + 4
-pygame.time.set_timer(mobAnimationTimer, randint(1000, 2000))
+mobAnimationTimer = py.USEREVENT + 4
+py.time.set_timer(mobAnimationTimer, randint(1000, 2000))
 
-mobAnimationTimer2 = pygame.USEREVENT + 5
-pygame.time.set_timer(mobAnimationTimer2, randint(1000, 2000))
+mobAnimationTimer2 = py.USEREVENT + 5
+py.time.set_timer(mobAnimationTimer2, randint(1000, 2000))
 
 # Game Loop.
 while True:
     screen.fill((0, 0, 0))
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
             exit()
             
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_ESCAPE:
+                py.quit()
                 exit()
 
         if gameActive:
             # Set Player Jump
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_w:
+            if event.type == py.KEYDOWN:
+                if event.key == py.K_SPACE or event.key == py.K_UP or event.key == py.K_w:
                     if playerRect.bottom == onGround:
                         jumpSound.play()
                         playerGravity = -20
                         
         else: 
             # Score Start Counter.
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
+            if event.type == py.KEYDOWN:
+                if event.key == py.K_RETURN:
                     gameActive = True
-                    startTime = int(pygame.time.get_ticks()/100)
+                    startTime = int(py.time.get_ticks()/100)
                     
         if gameActive:
             # Obstacle Spawn Random Position.
@@ -241,7 +259,7 @@ while True:
                     mobFramesIndex_L = 0
                 mobSurf_L = mobFrames_L[mobFramesIndex_L]
 
-    keys = pygame.key.get_pressed()
+    keys = py.key.get_pressed()
 
     # Set Player Idle Frame GameActive = 1.
     if right:
@@ -249,7 +267,7 @@ while True:
     elif left:
         playerImage = playerSurf_L
     
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+    if keys[py.K_RIGHT] or keys[py.K_d]:
         # Set Direction Bool
         right = True
         left = False
@@ -261,7 +279,7 @@ while True:
         # Speed Player Movement to Right
         playerRect.x += 7
         
-    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+    elif keys[py.K_LEFT] or keys[py.K_a]:
         # Set Direction Bool
         left = True
         right = False
@@ -325,18 +343,28 @@ while True:
         obsRectListL.clear()
         
         # Set Default Player Position.
-        playerRect.bottomleft = (WIDTH/2 - 24, onGround)
         playerGravity = 0
+        playerRect.bottomleft = (WIDTH/2 - 24, onGround)
         
         # Set Intro Text.
-        introSurf = overFont.render("D'D DEMONS", True, "white")
+        introSurf = overFont.render("GHOST YEETER", True, "white")
         introSurf.set_alpha(150)
         introRect = introSurf.get_rect(center= (WIDTH/2, HEIGHT/2 - 20))
         
-        # Set How to Start Game 
+        # Set Start Game Menu
         playSurf = fontPlay.render("press ENTER to play", True, "white")
         playSurf.set_alpha(150)
         playRect = playSurf.get_rect(center= (WIDTH/2, HEIGHT - 30))
+
+        # High Score Check
+        if score > highScore:
+            highScore = score
+        
+        # Set Highscore Menu
+        highSoreSurf = fontPlay.render(f"HIGH SCORE: {highScore}", True, "white")
+        highSoreSurf.set_alpha(150)
+        highSoreRect = highSoreSurf.get_rect(center= (WIDTH/2, 125))
+        highSoreRect2 = highSoreSurf.get_rect(center= (WIDTH/2, 68))
         
         # Set Text When Game Over.
         overSurf = overFont.render("GAME OVER", True, "white")
@@ -355,13 +383,14 @@ while True:
         
         if score == 0:
             # Display when Intro
-            screen.blit(introSurf, introRect)
-            screen.blit(playSurf, playRect)
+            introScreen()
         else:
             # Display when Game Over
-            screen.blit(overSurf, overRect)
-            screen.blit(scoreMsgSurf, scoreMsgRect)
-            screen.blit(playSurf2, playRect2)
+            gameOverScreen()
             
-    pygame.display.update()
+    py.display.update()
     clock.tick(60)
+    
+    f = open("data/high_score.txt", "w")
+    f.write(f"{highScore}")
+    f.close() 
